579dd2821cac7cf1fdf8493ebac6591f6f60c200
uablor <149031517+uablor@users.noreply.github.com>
2025-08-01 21:03:20 +0700

    update

--- src/infrastructure/database/seeders/seeds/seed-users.ts
+++ src/infrastructure/database/seeders/seeds/seed-users.ts
@@ -6,86 +6,78 @@ import { PermissionsEntity } from 'src/infrastructure/typeorm/permissions.orm-en
 import { RoleEntity } from 'src/infrastructure/typeorm/role.orm-entity';
 @Injectable()
 export class UsersSeeder {
-    constructor() { }
+  constructor() {}
 
-    async seed(manager: EntityManager) {
-        const _respository = manager.getRepository(UserEntity);
-        const permission = manager.getRepository(PermissionsEntity);
-        const role = manager.getRepository(RoleEntity);
-        const AllRoles = await role.find({ where: { name: 'super_admin' } });
-        const AllPermissions = await permission.find();
-        const items = [
-            {
-                name: 'super admin',
-                email: 'super_admin@gmail.com',
-                surname: 'super admin',
-                password: await hashPassword('super@1234'),
-                is_verified: true,
-                permissions: AllPermissions,
-                roles: AllRoles
+  async seed(manager: EntityManager) {
+    const _respository = manager.getRepository(UserEntity);
+    const permission = manager.getRepository(PermissionsEntity);
+    const role = manager.getRepository(RoleEntity);
+    const AllRoles = await role.find({ where: { name: 'super_admin' } });
+    const AllPermissions = await permission.find();
+    const items = [
+      {
+        email: 'super_admin@gmail.com',
+        username: 'super admin',
+        password: await hashPassword('super@1234'),
+        is_verified: true,
+        permissions: AllPermissions,
+        roles: AllRoles,
+      },
+      {
+        email: 'admin@gmail.com',
+        username: 'admin',
+        password: await hashPassword('admin@1234'),
+        is_verified: true,
+        permissions: AllPermissions,
+        roles: AllRoles,
+      },
+      {
+        email: 'phetAdmin@gmail.com',
+        username: 'Phet Admin',
+        password: await hashPassword('12345678phet'),
+        is_verified: true,
+        permissions: AllPermissions,
+        roles: AllRoles,
+      },
+      {
+        email: 'pao@gmail.com',
+        username: 'pao',
+        password: await hashPassword('12345678pao'),
+        is_verified: true,
+        permissions: AllPermissions,
+        roles: AllRoles,
+      },
+      {
+        email: 'oualor@gmail.com',
+        username: 'Oualor',
+        password: await hashPassword('12345678oualor'),
+        is_verified: true,
+        permissions: AllPermissions,
+        roles: AllRoles,
+      },
+      {
+        email: 'user@gmail.com',
+        username: 'user',
+        password: await hashPassword('user@1234'),
+        is_verified: true,
+        permissions: AllPermissions,
+        roles: AllRoles,
+      },
+    ];
 
-            },
-            {
-                name: 'admin',
-                email: 'admin@gmail.com',
-                surname: 'admin',
-                password: await hashPassword('admin@1234'),
-                is_verified: true,
-                permissions: AllPermissions,
-                roles: AllRoles
-
-            },
-            {
-                name: 'phet',
-                email: 'phetAdmin@gmail.com',
-                surname: 'Phet Admin',
-                password: await hashPassword('12345678phet'),
-                is_verified: true,
-                permissions: AllPermissions,
-                roles: AllRoles
-            },
-            {
-                name: 'pao',
-                email: 'pao@gmail.com',
-                surname: 'pao',
-                password: await hashPassword('12345678pao'),
-                is_verified: true,
-                permissions: AllPermissions,
-                roles: AllRoles
-            },
-            {
-                name: 'oualor',
-                email: 'oualor@gmail.com',
-                surname: 'Oualor',
-                password: await hashPassword('12345678oualor'),
-                is_verified: true,
-                permissions: AllPermissions,
-                roles: AllRoles
-            },
-            {
-                name: 'user',
-                email: 'user@gmail.com',
-                surname: 'user',
-                password: await hashPassword('user@1234'),
-                is_verified: true,
-                permissions: AllPermissions,
-                roles: AllRoles
-            },
-        ];
-
-        for (const item of items) {
-            const existingItem = await _respository.findOne({
-                where: { email: item.email },
-            });
-            if (!existingItem) {
-                const items = _respository.create(item);
-                await _respository.save(items);
-                console.log(`✅ Created User: ${item.email}`);
-            } else {
-                console.log(`⏩ Already exists: ${item.email}`);
-            }
-        }
-
-        console.log('🎉 User seeding complete.');
+    for (const item of items) {
+      const existingItem = await _respository.findOne({
+        where: { email: item.email },
+      });
+      if (!existingItem) {
+        const items = _respository.create(item);
+        await _respository.save(items);
+        console.log(`✅ Created User: ${item.email}`);
+      } else {
+        console.log(`⏩ Already exists: ${item.email}`);
+      }
     }
+
+    console.log('🎉 User seeding complete.');
+  }
 }

f45060d437b1a6a962f2f0d8a656c2284c764e76
uablor <149031517+uablor@users.noreply.github.com>
2025-08-12 13:32:59 +0700

    update attendance

--- src/modules/attendance/controller/attendance.controller.ts
+++ src/modules/attendance/controller/attendance.controller.ts
@@ -45,7 +45,7 @@ export class AttendanceController {
   async create(
     @Body() dto: CreateAttendanceDto,
     @CurrentUser() user: ProfiledDto,
-  ): Promise<Attendance> {
+  ): Promise<Attendance | string> {
     return this.createAttendUseCase.execute(dto, user.sub);
   }
 

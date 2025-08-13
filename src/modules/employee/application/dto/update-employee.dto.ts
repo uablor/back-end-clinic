3530fc71e05ea5f3b2ba672934ac3becea5f0b30
uablor <149031517+uablor@users.noreply.github.com>
2025-08-13 12:32:10 +0700

    udate employee

--- src/modules/employee/application/dto/update-employee.dto.ts
+++ src/modules/employee/application/dto/update-employee.dto.ts
@@ -1,3 +1,6 @@
-import { PartialType } from "@nestjs/mapped-types";
+
+import { OmitType, PartialType } from "@nestjs/mapped-types";
 import { CreateEmployeeDto } from "./create-employee.dto";
-export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {}
+
+export class UpdateEmployeeDto extends PartialType(OmitType(CreateEmployeeDto, ['password'] as const)) {}
+
